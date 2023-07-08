265519ddfa45182dc3f27e366e495181b7e1baab
kumarbek <kumarbektolonbayev@gmail.com>
2023-07-08 20:14:07 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Triangle j = new Triangle();
+        j.a = 12;
+        j.b = 16;
+        j.c = 13;
+        j.area();
+    }
+}
---
+++ src/Triangle.java
@@ -0,0 +1,13 @@
+public class Triangle {
+    int a;
+    int b;
+    int c;
+
+    public void area() {
+        int p = (a + b + c)/2;
+        int s = p*(p-a)*(p-b)*(p-c);
+        System.out.println(Math.sqrt(s));
+
+    }
+}
+
